a21f3ad38f0cc3007856800a84c120dfc5ceaab1
mamedzavr <mamedzavr@yandex.ru>
2021-10-15 12:59:20 +0300

    html pages added

---
+++ src/main/java/com/telekom/ecare/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.telekom.ecare.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/**");
+    }
+}
--- src/main/java/com/telekom/ecare/controller/HomeController.java
+++ src/main/java/com/telekom/ecare/controller/HomeController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping({"/", "/home"})
+    @GetMapping({"/", "/home", "/index"})
     public String home() {
-        return "home";
+        return "index";
     }
 }
